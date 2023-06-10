de410d1028d1bf8fde3944cf6253e05c4ccb9029
adnan106 <mdrakibulhasanadnan@gmail.com>
2023-06-10 15:20:50 +0600

    homepage edited by adnan

---
+++ app/src/main/java/com/example/cardiacrecorder/HomePage.java
@@ -0,0 +1,51 @@
+package com.example.cardiacrecorder;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class HomePage extends AppCompatActivity {
+
+    CardView add_card,record_card,profile_card,logout_card;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_homepage);
+
+        add_card=findViewById(R.id.add_cardview);
+        record_card = findViewById(R.id.records_cardview);
+        profile_card = findViewById(R.id.profile_cardview);
+        logout_card = findViewById(R.id.logout_cardview);
+
+        add_card.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        record_card.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        profile_card.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        logout_card.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+}
