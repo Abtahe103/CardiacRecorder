69a6ea2f4fdf8246fb2030f26ae13a40a8cc4bc2
adnan106 <mdrakibulhasanadnan@gmail.com>
2023-06-19 13:01:26 +0600

    data integrity constraints added to the fields in AddRecordActivity.

--- app/src/main/java/com/example/cardiacrecorder/AddRecordActivity.java
+++ app/src/main/java/com/example/cardiacrecorder/AddRecordActivity.java
@@ -4,7 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -13,6 +15,7 @@ import java.util.Locale;
 
 public class AddRecordActivity extends AppCompatActivity {
     EditText heart_rate_editText,diastolic_pressure_editText,systolic_pressure_editText,date_editText,time_editText;
+    Button addbutton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,6 +26,8 @@ public class AddRecordActivity extends AppCompatActivity {
         systolic_pressure_editText = findViewById(R.id.systolic_pressure_edit_text);
         date_editText = findViewById(R.id.date_edit_text);
         time_editText = findViewById(R.id.time_edit_text);
+
+        addbutton = findViewById(R.id.add_record_btn);
 
         date_editText.setOnFocusChangeListener(new View.OnFocusChangeListener() {
             @Override
@@ -47,7 +52,46 @@ public class AddRecordActivity extends AppCompatActivity {
                 }
             }
         });
+
+        addbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(date_editText.getError()==null){
+                    String enteredDate = date_editText.getText().toString().trim();
+                    if (!isValidDate(enteredDate)) {
+                        date_editText.setError("Invalid date");
+                    }
+                }
+                if(time_editText.getError()==null){
+                    String enteredTime = time_editText.getText().toString().trim();
+                    if (!isValidTime(enteredTime)) {
+                        time_editText.setError("Invalid time");
+                    }
+                }
+                if(heart_rate_editText.getText().length()==0||diastolic_pressure_editText.getText().length()==0||systolic_pressure_editText.getText().length()==0
+                        ||date_editText.getText().length()==0||time_editText.getText().length()==0||date_editText.getError()!=null
+                        ||time_editText.getError()!=null){
+                    if(heart_rate_editText.getText().length()==0){
+                        heart_rate_editText.setError("Required Field");
+                    }
+                    if(diastolic_pressure_editText.getText().length()==0){
+                        diastolic_pressure_editText.setError("Required Field");
+                    }
+                    if(systolic_pressure_editText.getText().length()==0){
+                        systolic_pressure_editText.setError("Required Field");
+                    }
+                    Toast.makeText(getApplicationContext(),"Please Fill out all the required fields in the given format",Toast.LENGTH_SHORT).show();
+                }
+
+                else{
+                    Toast.makeText(getApplicationContext(),"Record Added",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
     }
+
+
     private boolean isValidDate(String date1) {
         SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy", Locale.getDefault());
         sdf.setLenient(false);
